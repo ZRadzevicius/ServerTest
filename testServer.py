5c0f43088f4b75c4b787d1336193e9cf7e22bc51
ZRadzevicius <zilvinas.radzevicius94@gmail.com>
2017-05-02 15:59:02 +0000

    First commit

---
+++ testServer.py
@@ -0,0 +1,54 @@
+#! /usr/bin/python
+import sys
+import argparse
+import urllib2
+import socket
+import re
+
+parser = argparse.ArgumentParser(description='These are the arguments that must be supplied')
+parser.add_argument("-p", help="webserver port", required=True, type=int)
+parser.add_argument("-t", help="text to check in the body ", required=True)
+parser.add_argument('url', help="url of the website ", action="store")
+args = parser.parse_args()
+
+text = args.t
+port = args.p
+url = args.url
+
+try:
+    #create a socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+    print "FAIL"
+    sys.exit(0)
+
+try:
+    #get the IP address of the host
+    host_ip = socket.gethostbyname(url)
+except socket.gaierror:
+    #cant resolve host
+    print "FAIL" 
+    sys.exit(0)
+
+try:
+    #connecting to host with port specified
+    s.connect((host_ip,port))
+except socket.timeout:
+    print "FAIL"
+    sys.exit(0)
+
+try:
+    #Get content of the website
+    website = urllib2.urlopen('http://' + url).read()
+except urllib2.URLError:
+    print "FAIL"
+    sys.exit(0)
+
+#checking if text exists in website's content
+matches = re.findall(text, website);
+if len(matches) == 0:
+    print "FAIL"
+    sys.exit(0)
+else:
+    print "OK"
+    sys.exit(1)
